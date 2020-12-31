3ea21baa2c0daf302f685ab5f4d47438a34f9432
BattleBaxx <54268057+Ranga1503@users.noreply.github.com>
2021-01-01 01:51:48 +0530

    addded requests
    raz anime
    reckobs facts
    rod gals
    dawg doggo
    mechy katto

--- bot.js
+++ bot.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const Game = require('hangman-game-engine')
 const randomWords = require('random-words');
 const roasts = require('./roasts.js')
+const facts = require('fun-facts');
+const fetch = require('node-fetch');
 
 const MAX_GUESS = 8;
+var maintenance = false;
+var owner_id;
 
 const { exec } = require("child_process");
 const Discord = require('discord.js');
@@ -11,30 +15,80 @@ const client = new Discord.Client();
 let token = process.env.token;
 client.login(token);
 
+
 let hangmanGames = [];
 
 client.on('ready', readyDiscord);
 
-function readyDiscord(){
+async function readyDiscord(){
     console.log("Bot is Ready to Roll");
     client.user.setActivity("Press &help for help"); 
+    var promise = await client.fetchApplication();
+    owner_id = promise.owner.id;
+    // console.log( promise.owner.id);
 }
 
-const commands = ["&help", "&roast", "&bored", "&hi", "&hang"];
+const commands = ["&help", "&roast", "&bored", "&hi", "&hang", "&fact", "&gals", "&anime"];
 
 client.on('message', gotMessage);
 
-function gotMessage(msg){
+async function gotMessage(msg){
 
-    if(msg.channel.id !== "793403873951612931" && commands.includes(msg.content))
+    
+    if(msg.content == "&cease")
     {
-        msg.channel.send("`Dot the bot is under maintenance.`");
+        if(msg.author.id == owner_id)
+        {
+            await msg.channel.send("`Ceasing to exist in 3... 2... 1..`")
+            client.destroy();
+        }
+        else
+        {
+            msg.channel.send("`You dont have perms bruh.`")
+        }
         return;
+    }
+
+    if(msg.content == "&retire" )
+    {
+        if(msg.author.id == owner_id)
+        {
+            msg.channel.send("Maintenance mode: `On`")
+            maintenance = true;
+        }
+        else
+        {
+            msg.channel.send("`You dont have perms bruh.`")
+        }
+        return;
+    }
+
+    if(msg.content == "&arise" )
+    {
+        if(msg.author.id == owner_id)
+        {
+            msg.channel.send("Maintenance mode: `Off`")
+            maintenance = false;
+        }
+        else
+        {
+            msg.channel.send("`You dont have perms bruh.`")
+        }
+        return;
+    }
+
+    if(maintenance)
+    {
+        if(msg.channel.id !== "793403873951612931" && commands.includes(msg.content))
+        {
+            msg.channel.send("`Dot the bot is under maintenance.`");
+            return;
+        }
     }
 
     console.log(msg.content);
     if(msg.content === '&help')
-        msg.channel.send("```Commands u can use are &hi, &roast, &bored. Wanna know what they are? Fucking try for yourself.```")
+        msg.channel.send("```Commands u can use are &hi, &roast, &bored, &hang, &fact, &anime, &katto and &doggo. Try to know what they are....```")
 
     if(msg.content === '&hi')
         msg.reply('hi');
@@ -58,6 +112,54 @@ function gotMessage(msg){
             console.log(`stdout: ${stdout}`);
             msg.channel.send("```"+stdout+"```")
         });
+    }
+
+    if(msg.content === "&fact")
+    {
+        getFact = facts.get();
+        console.log(getFact);
+        msg.channel.send("`"+getFact.fact+"`")
+    }
+
+    if(msg.content == "&gals")
+    {
+        let url = `https://api.tenor.com/v1/search?q=hot+chick&${process.env.tenorKey}=LIVDSRZULELA`
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        let index = Math.floor(Math.random() * json.results.length)
+        msg.channel.send(json.results[index].url);
+    }
+
+    if(msg.content == "&anime")
+    {
+        let url = `https://api.tenor.com/v1/search?q=anime+cute&${process.env.tenorKey}=LIVDSRZULELA`
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        let index = Math.floor(Math.random() * json.results.length)
+        msg.channel.send(json.results[index].url);
+    }
+
+    if(msg.content == "&katto")
+    {
+        let url = `https://api.tenor.com/v1/search?q=kitten+cute&${process.env.tenorKey}=LIVDSRZULELA`
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        let index = Math.floor(Math.random() * json.results.length)
+        msg.channel.send(json.results[index].url);
+    }
+
+
+    if(msg.content == "&doggo")
+    {
+        let url = `https://api.tenor.com/v1/search?q=puppy+cute&${process.env.tenorKey}=LIVDSRZULELA`
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        let index = Math.floor(Math.random() * json.results.length)
+        msg.channel.send(json.results[index].url);
     }
 
     if(msg.content === '&hang') // initialise and start a new hangman game for the current user
@@ -110,7 +212,7 @@ function gotMessage(msg){
         if(currentGame.failedGuesses === MAX_GUESS)
         {
             currentGame.revealHiddenWord();
-            let reply = `Ofcourse you didnt win. Anyhow better luck next time NOT. The correct word was: ${currentGame.hiddenWord.join('')}`;
+            let reply = `You lost. Anyhow better luck next time. The correct word was: ${currentGame.hiddenWord.join('')}`;
             msg.reply("`" + reply + "`");
             delete hangmanGames[id];
             return;
@@ -118,7 +220,7 @@ function gotMessage(msg){
 
         if(!currentGame.hiddenWord.includes('*'))
         {
-            msg.reply("`Congrats you guessed the word, didn't think u would have done that.`");
+            msg.reply("`Cool you guessed it!`");
             delete hangmanGames[id];
             return;
         }
